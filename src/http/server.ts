ac8794bc2c2174c086658272a0c7120aedd52c42
Davidson William <davidsonwferreira8@gmail.com>
2024-02-12 17:13:26 -0300

    Finalizado a segunda aula

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,20 @@
-import fastify from "fastify"
-import { createPoll } from "../routes/create-poll"
+import fastify from "fastify";
+import { createPoll } from "../routes/create-poll";
+import { getPoll } from "../routes/get-poll";
+import { voteOnPoll } from "../routes/vote-on-poll";
+import cookie from "@fastify/cookie";
 
-const app = fastify()
+const app = fastify();
 
-app.register(createPoll)
+app.register(cookie, {
+  secret: "poll-app-nlw",
+  hook: 'onRequest',
+})
+
+app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3333 }).then(() => {
-  console.log('HTTP server running!!')
-})
+  console.log("HTTP server running!!");
+});
